6ee5a99f58c77cf59ae03bb586544c1c2da46d2c
enriquedlh97 <enriquedlh97@hotmail.com>
2023-03-28 23:48:53 -0600

    feat: Adds coinbot testing.

---
+++ coinbot/coinbot_test.py
@@ -0,0 +1,25 @@
+"""Coin bot testing."""
+import os
+from typing import Union
+
+from slack import WebClient
+
+from coinbot import CoinBot
+
+# Create a slack client
+slack_web_client: WebClient = WebClient(token=os.environ.get("SLACK_TOKEN"))
+
+# Get a new CoinBot
+coin_bot: CoinBot = CoinBot("#bot-dev")
+
+# Get the onboarding message payload
+message: dict[
+    str,
+    str
+    | list[
+        dict[str, Union[str, dict[str, str | tuple[str]]]],
+    ],
+] = coin_bot.get_message_payload()
+
+# Post the onboarding message in Slack
+slack_web_client.chat_postMessage(**message)
