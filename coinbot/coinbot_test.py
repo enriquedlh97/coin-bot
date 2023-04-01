aaf9ea69e606480ffe46cb9dda7884062d747b92
enriquedlh97 <34756365+enriquedlh97@users.noreply.github.com>
2023-04-01 12:39:20 -0600

    Flask app (#1)
    * feat: Adds Flask App.
    Bot working correctly with ngrok server.
    
    * docs: Updates `README`.
    
    * build: Updates dependencies.
    
    * refactor: Fixes issues with type annotations.
    
    * docs: Updates README.

--- coinbot/coinbot_test.py
+++ coinbot/coinbot_test.py
@@ -1,25 +1,21 @@
 """Coin bot testing."""
-import os
 from typing import Union
 
+from configs import config
 from slack import WebClient
 
 from coinbot import CoinBot
 
 # Create a slack client
-slack_web_client: WebClient = WebClient(token=os.environ.get("SLACK_TOKEN"))
+slack_web_client: WebClient = WebClient(token=config.SLACK_TOKEN)
 
 # Get a new CoinBot
-coin_bot: CoinBot = CoinBot("#bot-dev")  # Channel name
+coin_bot: CoinBot = CoinBot(config.APP_CONFIG.COINBOT_CHANNEL)  # Channel name
 
 # Get the onboarding message payload
 message: dict[
-    str,
-    str
-    | list[
-        dict[str, Union[str, dict[str, str | tuple[str]]]],
-    ],
+    str, str | list[dict[str, Union[str, dict[str, str]]]]
 ] = coin_bot.get_message_payload()
 
 # Post the onboarding message in Slack
-slack_web_client.chat_postMessage(**message)
+slack_web_client.chat_postMessage(**message)  # type: ignore
